bf398416ca2599d263130397f7bdbd6d65ac97a5
AlexSV <sav75.odessa@gmail.com>
2020-12-06 19:43:32 +0200

    Changed the class RersonDetails

--- src/module2/main/java/PersonalDetails.java
+++ src/module2/main/java/PersonalDetails.java
@@ -6,10 +6,35 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("What is your name : ");
+        System.out.print("What is your name: ");
         String name = in.nextLine();
-        System.out.printf("Name: %s \n", name);
-        in.close();
+        System.out.print("What city do you live in: ");
+        String city = in.nextLine();
+        System.out.print("How old are you: ");
+        int age = in.nextInt();
+        in.nextLine();
+        System.out.print("What is your hobby: ");
+        String hobby = in.nextLine();
 
+        System.out.println();
+        System.out.printf("Name:\t %s" +
+                          "\nCity:\t %s" +
+                          "\nAge:\t %d" +
+                          "\nHobby:\t %s",
+                          name, city, age, hobby);
+
+        System.out.println();
+        System.out.println();
+        System.out.printf("Man(Woman) named %s lives in the city %s" +
+                         "\nHe(She) is %d years old and lives doing %s", name, city, age, hobby);
+
+        System.out.println();
+        System.out.println();
+        System.out.printf("%s - Name" +
+                          "\n%s - City" +
+                          "\n%d - Age"  +
+                          "\n%s - Hobby",
+                          name, city, age, hobby);
+        in.close();
     }
 }
