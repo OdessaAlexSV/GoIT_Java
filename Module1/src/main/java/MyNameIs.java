3e9d7182777d287d906a7a3e5406c6df64268056
AlexSV <sav75.odessa@gmail.com>
2020-12-16 20:26:43 +0200

    Changes for MyNameIS class

--- Module1/src/main/java/MyNameIs.java
+++ Module1/src/main/java/MyNameIs.java
@@ -2,6 +2,6 @@ package Module1.src.main.java;
 
 public class MyNameIs {
     public static void main(String[] args) {
-        System.out.println("My name is: Alex");
+        System.out.println("Test");
     }
 }
