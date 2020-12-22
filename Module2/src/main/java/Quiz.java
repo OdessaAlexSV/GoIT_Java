84a66ea8361b292377c66e54a6f220ed6dbdac4a
AlexSV <sav75.odessa@gmail.com>
2020-12-22 14:57:22 +0200

    Added a new class Quiz.java for 2nd module

---
+++ Module2/src/main/java/Quiz.java
@@ -0,0 +1,31 @@
+package Module2.src.main.java;
+
+public class Quiz {
+    public static void main(String[] args) {
+        String variable = null;
+
+//        if (variable != null & !variable.isEmpty()) {
+//            System.out.println("not empty");
+//        } else {
+//            System.out.println("empty");
+//        }
+
+        if (variable == null || variable.isEmpty()) {
+            System.out.println("empty");
+        } else {
+            System.out.println("not empty");
+        }
+
+//        if (variable == null | variable.isEmpty()) {
+//            System.out.println("empty");
+//        } else {
+//            System.out.println("not empty");
+//        }
+
+        if (variable != null && !variable.isEmpty()) {
+            System.out.println("not empty");
+        } else {
+            System.out.println("empty");
+        }
+    }
+}
