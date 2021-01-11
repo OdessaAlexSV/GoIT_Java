d0f1788b257db99508563b90253706884e0667ec
AlexSV <sav75.odessa@gmail.com>
2021-01-11 17:59:32 +0200

    Added new class VarTask to the Module#2

---
+++ Module2/src/main/java/botTasks/VarTask_6.java
@@ -0,0 +1,24 @@
+package main.java.botTasks;
+
+class VarTask_6 {
+    public static void main(String[] args) {
+        byte happyValue = 8;
+        short cold = -50;
+        int globalAnswer = 42;
+        int randomAnswer;
+        randomAnswer = 700;
+        long longerThanLife = 223372036869l;
+        float tooBigToFail = 1025.5f;
+        double muchBiggerThanTooBig = -3.4028234663852886E38;
+        //double muchBiggerThanTooBig = -(Float.MAX_VALUE+1);
+
+        //Example output
+        System.out.println("happyValue = " + happyValue);
+        System.out.println("cold = " + cold);
+        System.out.println("globalAnswer = " + globalAnswer);
+        System.out.println("randomAnswer = " + randomAnswer);
+        System.out.println("longerThanLife = " + longerThanLife);
+        System.out.println("tooBigToFail = " + tooBigToFail);
+        System.out.println("muchBiggerThanTooBig = " + muchBiggerThanTooBig);
+    }
+}
